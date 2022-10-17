8a5df7df0bcf6cefb6089bec7889f639367285a0
Daniel Knaack <daniel.knaack@protonmail.com>
2022-10-17 16:16:54 +0000

    add binding for toggling floating

--- config.h
+++ config.h
@@ -76,6 +76,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_e,         setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,     setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,     togglefloating, {0} },
+	{ MODKEY|ControlMask,           XK_f,         togglefloating, {0} },
 	{ MODKEY,                       XK_0,         view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,         tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,     focusmon,       {.i = -1 } },
